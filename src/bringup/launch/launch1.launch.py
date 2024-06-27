1441552901c27a4ccb642348266826583200f2bb
simon <rsimoon@gmail.com.com>
2024-06-27 18:25:52 +0200

    first working launch

--- src/bringup/launch/launch1.launch.py
+++ src/bringup/launch/launch1.launch.py
@@ -24,32 +24,44 @@ def generate_launch_description():
     use_sim_time = PythonExpression(["'true'" if simu else "'false'"])
 
 #========================================== ROBOT_DESCRIPTION ===========================================
+    context = LaunchContext()
+    
+    robot_description_path = PathJoinSubstitution(
+           [FindPackageShare(robot_model_config_pkg), "urdf", "robot.urdf"])
+        
+    # robot_description_path = PathJoinSubstitution(
+    #        [FindPackageShare(robot_model_config_pkg), config_dir_name, f"{robot_name}_motoros.urdf.xacro"])
+
+    robot_description_config = xacro.process_file(evaluate_substitution(context, robot_description_path))
+    robot_description = {"robot_description": robot_description_config.toxml()}
 
     robot_description_arg = DeclareLaunchArgument(
         'robot_description',
-        default_value=PathJoinSubstitution([FindPackageShare(robot_model_config_pkg), "urdf", "robot.urdf"]),
+        default_value=robot_description,
         description='Absolute path to robot urdf file'
     )
+
     log_robot_description_path = LogInfo(msg=LaunchConfiguration('robot_description'))
 
      # Node to publish the state of the joints
     JSP=Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
         name='joint_state_publisher',
         output='screen',
         parameters=[{'use_gui': True}],
     )
 
     # Node to publish the state of the robot to tf
+    RSP_freq=DeclareLaunchArgument("publish_frequency", default_value="15.0")
     RSP=Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        
         parameters=[{'use_sim_time': use_sim_time},
-                    {'robot_description': LaunchConfiguration('robot_description')} ],
+                    {"robot_description": robot_description_config.toxml()},
+                    {"publish_frequency": LaunchConfiguration("publish_frequency")},],
     )
     
     # Node to launch RViz
@@ -62,6 +74,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        RSP_freq,
         robot_description_arg,
         log_robot_description_path,
         JSP,
